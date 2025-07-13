dc075ead0f3edb3dd5c615c721bbe05ee7e9cec7
April Hall <ari@arithefirst.com>
2025-07-13 19:38:52 -0400

    feat: Added color flag and color validation logic

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
 func main() {
 	  flags := parseFlags()
 		config := parseConfig(flags)
+
+		color, colorExists := config.Colors[flags.Color]
+
+		if flags.Color == "N/A" {
+			log.Fatalln("The \"-color\" flag must be set.")
+		} else if !colorExists {
+			log.Fatalf("The Color \"%s\" does not exist in your config (%s)", flags.Color, flags.ConfigPath)
+		}
+
+		fmt.Println(color)
 }
--- parseFlags.go
+++ parseFlags.go
@@ -4,6 +4,7 @@ import "flag"
 
 type Flags struct {
 	ConfigPath string
+	Color string
 }
 
 func parseFlags() Flags {
@@ -11,10 +12,13 @@ func parseFlags() Flags {
 	
 	// Get flags
 	configPath := flag.String("config", "~/.config/palettro/config.json", "Sets the path to the config file")
+	color := flag.String("color", "N/A", "New accent color to use for your RICE")
+
 	flag.Parse()
 	
 	// Populate struct
 	returnFlags.ConfigPath = *configPath
+	returnFlags.Color = *color
 
 	return returnFlags
 }
